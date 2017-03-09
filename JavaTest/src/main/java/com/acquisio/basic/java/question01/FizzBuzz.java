6ae8d8fce3c0efd3cb8fcb2f8b50cbf799f41593
ekowpo <ekowpo@gmail.com>
2017-03-09 14:34:42 -0500

    Update FizzBuzz.java
    solution to FizzBuzz

--- JavaTest/src/main/java/com/acquisio/basic/java/question01/FizzBuzz.java
+++ JavaTest/src/main/java/com/acquisio/basic/java/question01/FizzBuzz.java
@@ -1,5 +1,7 @@
 package com.acquisio.basic.java.question01;
 
+//import concurrency.FizzBuzz;
+
 /**
  * QUESTION 01: FizzBuzz
  * You have a program that receives a value, which we assume is a positive
@@ -8,19 +10,50 @@ package com.acquisio.basic.java.question01;
  * <p>
  * The program must follow those rules:
  * - Must not print anything if the value cannot be divided by itself.
- * - Must print ‘Fizz’ if the value can be divided by 3.
- * - Must print ‘Buzz’ if the value can be divided by 5.
+ * - Must print â€˜Fizzâ€™ if the value can be divided by 3.
+ * - Must print â€˜Buzzâ€™ if the value can be divided by 5.
  * - Otherwise, print the current integer value in the loop.
  *
  * IMPORTANT: Add all missing javadoc that you think is necessary.
  */
+
 public class FizzBuzz {
     public static void main(String[] args) {
         FizzBuzz main = new FizzBuzz();
         main.fizzBuzz(50);
     }
 
+    /**
+     * fizzbuzz method
+     * @param maxValue Max value
+     */
     private void fizzBuzz(int maxValue) {
-        // TODO: Insert your code here
+       
+    	for(int i= 0; i<=maxValue; i++)
+    	{
+    		try
+    		{
+    	
+    		if((i%3)==0)
+    		{
+    			
+    			System.out.print("â€˜Fizzâ€™");
+    		}
+    		if((i%5)==0)
+    		{
+    			System.out.print("â€˜Buzzâ€™");
+    		}
+    		if((i%i)==0)
+    		{
+    			System.out.print(i);
+    		}
+    		System.out.println("");
+    		}
+    		catch(ArithmeticException ex)
+    		{
+    			
+    		}
+    		
+    	}	
     }
 }
