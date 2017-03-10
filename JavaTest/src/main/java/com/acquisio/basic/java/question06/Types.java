b818fc7ee03840c47adb88425ab93b10b1ad5b4d
ekowpo <ekowpo@gmail.com>
2017-03-10 18:36:01 -0500

    Update Types.java
    Documentation to Types.

--- JavaTest/src/main/java/com/acquisio/basic/java/question06/Types.java
+++ JavaTest/src/main/java/com/acquisio/basic/java/question06/Types.java
@@ -1,17 +1,32 @@
 package com.acquisio.basic.java.question06;
 
+/**
+ * Enum of Customer types
+ * @author Ekow-Daniels
+ *
+ */
 public enum Types {
 	enterprise(1.10d), 
 	professional(1.20d),
 	Private(1.30d);
 	
+	
 	private double value;
 	
+	/**
+	 * Types Constructor
+	 * @param value type corresponding value.
+	 */
 	private Types(double value)
 	{
 		this.value = value;
 	}
 	
+	
+	/**
+	 * This method returns the value of type
+	 * @return value
+	 */
 	public double getValue()
 	{
 		return value;
