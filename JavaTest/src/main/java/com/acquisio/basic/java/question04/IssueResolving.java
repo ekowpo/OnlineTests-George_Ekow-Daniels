1fff7438bbecc1aab7007c5f42853ae482fdb10f
ekowpo <ekowpo@gmail.com>
2017-03-13 12:37:34 -0400

    Update IssueResolving.java
    solution to question 4

--- JavaTest/src/main/java/com/acquisio/basic/java/question04/IssueResolving.java
+++ JavaTest/src/main/java/com/acquisio/basic/java/question04/IssueResolving.java
@@ -13,9 +13,16 @@ public class IssueResolving {
         System.out.println(instance.factorial(7));
     }
 
+    /**
+     * This method calculates the factorial of a given number
+     * @param n number 
+     * @return factorial
+     */
     private int factorial(int n) {
         // TODO: Fix code here.
-        int result = 0;
+    	int result = 0;
+    	if(n==1)
+    		return 1;
         result = factorial(n - 1) * n;
         return result;
     }
