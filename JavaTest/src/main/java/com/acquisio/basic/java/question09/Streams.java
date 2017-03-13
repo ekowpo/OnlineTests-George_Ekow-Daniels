899dba1fb37075282af6ea1e975d3d9467c8ab20
ekowpo <ekowpo@gmail.com>
2017-03-13 12:12:00 -0400

    Update Streams.java
    Solution to Stream

--- JavaTest/src/main/java/com/acquisio/basic/java/question09/Streams.java
+++ JavaTest/src/main/java/com/acquisio/basic/java/question09/Streams.java
@@ -1,9 +1,16 @@
 package com.acquisio.basic.java.question09;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 /**
  * QUESTION 09: Streams (https://docs.oracle.com/javase/8/docs/api/java/util/stream/package-summary.html)
@@ -34,6 +41,99 @@ public class Streams {
 
     private void convertCarts(File input, File output) throws IOException {
         // TODO: Insert your code here.
+    	
+    	
+    	Files.lines(input.toPath())
+    	.map(line ->mapToItem(line))
+    	.filter(data -> data.getAmount()<50.0)
+    	.forEach(data->writeToFile(data,output));
+    
     }
 
+    /**
+     * This method writes to the output file 
+     * @param data
+     * @param output
+     */
+ public  void writeToFile(CartItems data, File output )
+ {
+	 
+	 try(BufferedWriter write = new BufferedWriter(new FileWriter(output))) {
+		write.write(data.getUser()+","+data.getAmount()+","+data.getNbItems()+","+data.getTaxes()+","+data.getTotal()+"\n");
+	
+		write.flush();
+		write.close();
+	 } catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	 
+ }
+ /**
+  * To method maps each line to an item
+  * @param line CSV record line
+  * @return an object of item
+  */
+    public  CartItems mapToItem(String line){
+    	String delimitor = ",";
+    	  String[] p = line.split(delimitor);
+    	  CartItems item = new CartItems();
+    	  item.setUser(p[0]);
+    	  item.setAmount(Double.parseDouble(p[1]));
+    	  item.setNbItems(p[3]);
+    	  item.setTaxes();
+    	  item.setTotal();
+    	  return item;
+    	};
+
+}
+
+/**
+ * iCartItem class to hold the line data 
+ * @author Ekow-Daniels
+ *
+ */
+ class CartItems {
+	 private String User;
+	 private double Amount;
+	 private String NbItems;
+	 private double taxes;
+	 
+	 private double Total;
+	
+	 
+	 public double getTaxes() {
+		return taxes;
+	}
+	public void setTaxes() {
+		this.taxes = this.getAmount()*0.15;
+	}
+	public String getUser() {
+		return User;
+	}
+	public void setUser(String user) {
+		User = user;
+	}
+	public double getAmount() {
+		return Amount;
+	}
+	public void setAmount(double amount) {
+		Amount = amount;
+	}
+	
+	public String getNbItems() {
+		return NbItems;
+	}
+	public void setNbItems(String nbItems) {
+		NbItems = nbItems;
+	}
+	public double getTotal() {
+		return Total;
+	}
+	public void setTotal() {
+		Total = this.getAmount()+this.getTaxes();
+	}
+	 
+	 
+	
 }
