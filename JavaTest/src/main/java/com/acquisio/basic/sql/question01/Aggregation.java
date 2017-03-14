5b91213091c4d88553b9ff3f24060292b37cfc43
ekowpo <ekowpo@gmail.com>
2017-03-13 20:47:21 -0400

    Update Aggregation.java
    Aggregation completed

--- JavaTest/src/main/java/com/acquisio/basic/sql/question01/Aggregation.java
+++ JavaTest/src/main/java/com/acquisio/basic/sql/question01/Aggregation.java
@@ -50,7 +50,8 @@ public class Aggregation {
 
             // TODO: Insert query here
             // See requirement in this class javadoc
-            String query = "select 1 as dummyValue from dual";
+            String query = "select projects.title as ProjectTitle, COALESCE(Count(employees_projects.employee_id),0) as NumberOfEmployees from employees_projects left join projects on  employees_projects.project_id=projects.id group by projects.title";
+            ;
 
             ResultSet resultSet = conn.createStatement().executeQuery(query);
             H2DBUtil.displayResultSet(resultSet);
