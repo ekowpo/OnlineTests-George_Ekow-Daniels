7a1abdff0c62797576f3fffb45a46669a65a8e9e
ekowpo <ekowpo@gmail.com>
2017-03-13 20:59:12 -0400

    Update Debugging.java
    Debugging completed. Used two list. navigated one and removed the element in the other. At the end returned the removed list

--- JavaTest/src/main/java/com/acquisio/basic/java/question07/Debugging.java
+++ JavaTest/src/main/java/com/acquisio/basic/java/question07/Debugging.java
@@ -19,17 +19,22 @@ public class Debugging {
         System.out.println(instance.removeEvenNumbers(1, 2, 3, 4));
     }
 
+    /**
+     * This method removes Even numbers from a list of numbers
+     * @param values a varargs of integers
+     * @return a list of non-even numbers
+     */
     private List<Integer> removeEvenNumbers(Integer ... values) {
         // TODO: Fix code here.
 
         List<Integer> integers = new ArrayList<>(Arrays.asList(values));
-
+        List<Integer> removedIntegers = new ArrayList<>(Arrays.asList(values));
         for (Integer i : integers) {
-            if (i % 2 == 0) { // remove even numbers
-                integers.remove(i);
+            if (i.intValue() % 2 == 0) { // remove even numbers
+            	removedIntegers.remove(i);
             }
         }
 
-        return integers;
+        return removedIntegers;
     }
 }
